d5997056112b38011fc3e1bdbf1857a9a33b210e
Kunal Krishna <kunalkrishna85@gmail.com>
2025-04-27 16:24:54 -0400

    modified to save output in a file

--- jpf-core/src/project/BankersVerifier.java
+++ jpf-core/src/project/BankersVerifier.java
@@ -1,8 +1,5 @@
 import gov.nasa.jpf.vm.Verify;
 import java.util.Arrays;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
 
 public class BankersVerifier {
 
@@ -19,8 +16,6 @@ public class BankersVerifier {
         for (int j = 0; j < R; j++) {
             available[j] = Verify.getInt(0, 3);
         }
-//        System.out.println("Available : ");
-//        System.out.println(Arrays.toString(available));
 
         for (int i = 0; i < P; i++) {
             for (int j = 0; j < R; j++) {
@@ -29,13 +24,15 @@ public class BankersVerifier {
                 need[i][j] = max[i][j] - allocation[i][j];
             }
         }
-//        System.out.println("Max : ");
-//        printMatrix(max);
-//        System.out.println("Allocation : ");
-//        printMatrix(allocation);
-//        System.out.println("Need : ");
-//        printMatrix(need);
-//        System.out.println("-------");
+
+        System.out.println("Available : ");
+        System.out.println(Arrays.toString(available));
+        System.out.println("Max : ");
+        printMatrix(max);
+        System.out.println("Allocation : ");
+        printMatrix(allocation);
+        System.out.println("Need : ");
+        printMatrix(need);
 
         // Invariant check: need = max - allocation
         for (int i = 0; i < P; i++) {
@@ -49,17 +46,22 @@ public class BankersVerifier {
         // Now we verify: if reported safe, there MUST exist a safe sequence
         if (safe) {
             assert validateSafeSequence(available, allocation, need) : "False positive: claimed safe but no sequence exists!";
+            System.out.println("[Safe Sequence Validated]");
+        } else {
+            System.out.println("*** Unsafe to process for given input config ***");
         }
+        System.out.println("-------");
     }
 
     // Implementation of the Banker's safety algorithm
     static boolean isSafe(int[] available, int[][] allocation, int[][] need) {
-        int[] work = Arrays.copyOf(available, R);
+        int[] work = Arrays.copyOf(available, R);//copyOf(int[] original, int newLength)
         boolean[] finish = new boolean[P];
         boolean progress;
 
         do {
             progress = false;
+            //can at least 1 process be completed?
             for (int i = 0; i < P; i++) {
                 if (!finish[i] && canProceed(need[i], work)) {
                     // Simulate process completion
